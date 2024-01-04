368d6d9a1b4f1e8a1829d16972f2965cf1d1e215
Shivam Singh <shivamdotsingh@gmail.com>
2024-01-04 19:17:38 +0530

    code added

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db/db'); 
+const app = express();
+require('dotenv').config();
+const {readdirSync} = require('fs')
+const PORT = process.env.PORT;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+//routes 
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+const server = () => {
+    db();
+    app.listen(PORT, () => {
+        console.log("You are listening to port: ", PORT);
+    });
+};
+
+server();
